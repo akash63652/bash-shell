d0b89f7b04fd63e1eac4be54ef8a20d8ee566a56
akash63652 <131855624+akash63652@users.noreply.github.com>
2024-03-10 19:50:26 +0530

    Add files via upload

---
+++ bash/bash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#echo "hello there is" $1
+#echo "hello there is" $2
+#echo "hello there is" $@
+
+#echo "what is your name?"
+#read name
+
+#echo "there is $name"
+#echo "welcome to $name"
+
+#read -p "what is your name? " name
+
+#echo "hi there is $name"
+#echo "welocome to $name sir"
+
+my_array=("value 1" "value 2" "value 3" "value 4")
+echo ${my_array[@]}
+
